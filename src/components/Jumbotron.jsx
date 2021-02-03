3c83d175fdc49b2526ef1e78aa77b68c1f2f3382
tdenise <t.denise44@yahoo.com>
2021-02-03 10:02:57 -0800

    added jumbotron

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export function Jumbo() {
+    return (
+        <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to featured content or information.
+        </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
+        </Jumbotron>
+    )
+}
